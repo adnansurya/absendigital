d97198fc11dd58912f800fbac4cfec2f7b6f57b9
Adnan <muh.adnansuryaazis@gmail.com>
2022-12-26 10:11:44 +0800

    ini file

---
+++ public/js/secret.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAb_qFLS4TGXd3u0RcVfLzmfEL2_7-zyoA",
+    authDomain: "absensmpn6mks.firebaseapp.com",
+    databaseURL: "https://absensmpn6mks-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    projectId: "absensmpn6mks",
+    storageBucket: "absensmpn6mks.appspot.com",
+    messagingSenderId: "436658423076",
+    appId: "1:436658423076:web:2b0f6b4858c065e82c9fb1"
+  };
